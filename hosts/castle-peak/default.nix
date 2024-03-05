1007207694b6123a5e0706b657868a4510333c77
Dennis Dervisis <6106539+ddervisis@users.noreply.github.com>
2024-03-05 16:02:50 +0100

    Add additional headless host

---
+++ hosts/castle-peak/default.nix
@@ -0,0 +1,39 @@
+{ nixpkgs, nur, config, lib, vars, hostName, system, ... }:
+
+let
+  pkgs = import nixpkgs {
+    inherit system;
+    config.allowUnfree = true;
+  };
+in {
+  imports = [ (import ./hardware-configuration.nix) ] ++
+  ++ (import ../../modules/virtualisation);
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    loader = {
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 3;
+      };
+      efi.canTouchEfiVariables = true;
+      timeout = 3;
+    };
+  };
+
+  networking = {
+    useDHCP = lib.mkDefault true;
+    hostName = hostName;
+    networkmanager.enable = true;
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 22 ];
+    };
+  };
+
+  environment = {
+    systemPackages = with pkgs; [
+      ripgrep
+    ];
+  };
+}
