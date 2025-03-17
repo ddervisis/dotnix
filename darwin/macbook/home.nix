f8c936f6dc5582ca0c507e2cee7809035e829963
Dennis Dervisis <6106539+ddervisis@users.noreply.github.com>
2025-03-17 12:03:20 +0100

    Move macbook specific zsh init config to its own configuration file

--- darwin/macbook/home.nix
+++ darwin/macbook/home.nix
@@ -6,4 +6,14 @@
   stateVersion,
   ...
 }: {
+  programs = {
+    zsh = {
+      initExtra = ''
+        alias docker="podman"
+        if [ -f ~/.pypi.sh ]; then
+          source ~/.pypi.sh
+        fi
+      '';
+    };
+  };
 }
